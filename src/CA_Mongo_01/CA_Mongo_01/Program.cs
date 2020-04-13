9922828c68258670b3bbe19366c8f936e60b2bc2
Nhoppasit S <39229469+nhoppasit@users.noreply.github.com>
2020-04-14 06:21:31 +0700

    Initial commit

---
+++ src/CA_Mongo_01/CA_Mongo_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Mongo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("myFristDb");
+            var collection = db.GetCollection<Student>("students");
+            Student student = new Student("Nhoppasit", "Srisurat");
+            collection.InsertOne(student);
+        }
+    }
+}
---
+++ src/CA_Mongo_01/CA_Mongo_01/Student.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Mongo_01
+{
+    class Student
+    {
+        public ObjectId Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+
+        public Student(string first_Name, string last_Name)
+        {
+            First_Name = first_Name;
+            Last_Name = last_Name;
+        }
+    }
+}
